12c80998d6b269c12dd5c956a620f74d9a6bcb2f
Sergey Abakumov <Sabakumov@binck.nl>
2020-12-24 13:13:11 +0100

    add arjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import {
+  AR_EXPORTS,
+  HtmlExportService,
+  PdfExportService,
+  ViewerComponent,
+  XlsxExportService,
+} from "@grapecity/activereports-angular";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  template:
+    '<div id="viewer-host"><gc-activereports-viewer (init)="onViewerInit()"> </gc-activereports-viewer></div> ',
+  styleUrls: ["./app.component.scss"],
+  providers: [
+    {
+      provide: AR_EXPORTS,
+      useClass: PdfExportService,
+      multi: true,
+    },
+    {
+      provide: AR_EXPORTS,
+      useClass: HtmlExportService,
+      multi: true,
+    },
+    {
+      provide: AR_EXPORTS,
+      useClass: XlsxExportService,
+      multi: true,
+    },
+  ],
 })
 export class AppComponent {
-  title = 'arjs-angular8-azure-deploy';
+  @ViewChild(ViewerComponent, { static: false }) reportViewer: ViewerComponent;
+  onViewerInit() {
+    this.reportViewer.open("assets/report.rdlx-json");
+  }
 }
